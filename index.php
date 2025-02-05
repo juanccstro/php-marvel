36567d0fb926a1f7095d1a88daf539954ec2270a
Juan Carlos Castro <153071018+juanccstro@users.noreply.github.com>
2025-02-05 13:28:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ curl_close($ch);
 	<meta charset="UTF-8" />
 	<title>La próxima película de Marvel</title>
 	<meta name="description" content="La próxima película de Marvel" />
-	<meta name="viewport" content="whidth=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link
 		rel="stylesheet"
 		href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css" />
